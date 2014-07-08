57911bec9e552671e897094f828c243d82c77f88
Nathan Handler <nhandler@enova.com>
2014-07-08 10:30:49 -0500

    Move FB APP_ID and APP_SECRET to ENV variables

--- app/assets/javascripts/facebook.js.coffee
+++ app/assets/javascripts/facebook.js.coffee
@@ -8,7 +8,7 @@ jQuery ->
 
 
 window.fbAsyncInit = ->
-  FB.init(appId: '538104199648431', cookie: true)
+  FB.init(appId: <%= ENV['FACEBOOK_APP_ID'] >, cookie: true)
 
   $('#sign_in').click (e) ->
     e.preventDefault()
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, '538104199648431', '71e0004484cc7cb707cbe812a3e92657'
+    provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
 end
