e4d3c444f7a31a68a53aad3f18e95212a6a0c7ea
Nathan Handler <nhandler@enova.com>
2014-07-08 11:40:46 -0500

    Update user available codes on login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,8 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
+    user.available_codes = user.get_available
+    user.save
     redirect_to root_url
   end
 
