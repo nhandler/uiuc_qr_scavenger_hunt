7b9985d53983b3093284d736f312fceeab0237c6
Nathan Handler <nhandler@enova.com>
2014-07-08 13:46:34 -0500

    Fix error if User table dropped while user is logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,9 @@ class ApplicationController < ActionController::Base
 
   private
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id]) rescue nil
+    end
   end
   helper_method :current_user
 end
